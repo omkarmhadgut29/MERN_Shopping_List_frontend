9e2513c4f0965cad7cb38e7c4c88b09d6fa58d5b
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-29 19:03:03 +0530

    PROXY

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,30 @@ import react from "@vitejs/plugin-react";
 
 // // https://vitejs.dev/config/
 
-// export default defineConfig({
-//     server: {
-//         proxy: {
-//             "/api/items": {
-//                 target: import.meta.env.VITE_BACKEND_SERVER,
-//                 changeOrigin: true,
-//             },
-//         },
-//     },
-//     plugins: [react()],
-// });
-
-export default ({ mode }) => {
-    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
-
-    return defineConfig({
-        server: {
-            proxy: {
-                "/api/items": {
-                    target: process.env.VITE_BACKEND_SERVER,
-                    changeOrigin: true,
-                },
+export default defineConfig({
+    server: {
+        proxy: {
+            "/api/items": {
+                target: import.meta.env.VITE_BACKEND_SERVER,
+                changeOrigin: true,
             },
         },
-        plugins: [react()],
-    });
-};
+    },
+    plugins: [react()],
+});
+
+// export default ({ mode }) => {
+//     process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+//     return defineConfig({
+//         server: {
+//             proxy: {
+//                 "/api/items": {
+//                     target: process.env.VITE_BACKEND_SERVER,
+//                     changeOrigin: true,
+//                 },
+//             },
+//         },
+//         plugins: [react()],
+//     });
+// };
