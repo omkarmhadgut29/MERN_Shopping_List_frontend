005529e68b0e08e6494b6f71369546cf7e6626a0
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-29 18:48:04 +0530

    first commit

---
+++ src/components/ShoppingList.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import { Button, Container, ListGroup, ListGroupItem } from "reactstrap";
+import { CSSTransition } from "react-transition-group";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    deleteItem,
+    getItems,
+    loadItems,
+    setItemLoading,
+} from "../features/itemSlice";
+import axios from "axios";
+
+function ShoppingList() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setItemLoading());
+        axios.get("/api/items").then((res) => {
+            dispatch(loadItems(res.data));
+        });
+    }, [dispatch]);
+
+    const items = useSelector(getItems);
+
+    return (
+        <Container>
+            <ListGroup>
+                {items.map(({ _id, name }) => {
+                    return (
+                        <CSSTransition
+                            key={_id}
+                            timeout={500}
+                            classNames="fade"
+                        >
+                            <ListGroupItem>
+                                <Button
+                                    className="remove_btn"
+                                    color="danger"
+                                    onClick={() => {
+                                        dispatch(deleteItem(_id));
+                                    }}
+                                    size="sm"
+                                >
+                                    &times;
+                                </Button>
+                                {name}
+                            </ListGroupItem>
+                        </CSSTransition>
+                    );
+                })}
+            </ListGroup>
+        </Container>
+    );
+}
+
+export default ShoppingList;
